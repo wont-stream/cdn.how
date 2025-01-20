ec06a3f6423f8bc6d4687ab0726b675bf5364916
Mirco Wittrien <mirco.wittrien@gmail.com>
2025-01-20 16:42:20 -0500

    Formatting. - DevilBro

--- build.ts
+++ build.ts
@@ -1,20 +1,20 @@
 const build = async (minify: boolean) => {
-    const start = Date.now();
-    const built = await Bun.build({
-        entrypoints: ["./src/index.html"],
-        outdir: "./dist",
-        html: true,
-        experimentalCss: true,
-        splitting: true,
-        sourcemap: "linked",
-        minify,
-        loader: {
-            ".woff2": "file",
-        }
-    });
-    const end = Date.now();
-    console.log("Did build", built.success, built.logs);
-    console.log("Build time:", end - start, "ms");
+	const start = Date.now();
+	const built = await Bun.build({
+		entrypoints: ["./src/index.html"],
+		outdir: "./dist",
+		html: true,
+		experimentalCss: true,
+		splitting: true,
+		sourcemap: "linked",
+		minify,
+		loader: {
+			".woff2": "file",
+		},
+	});
+	const end = Date.now();
+	console.log("Did build", built.success, built.logs);
+	console.log("Build time:", end - start, "ms");
 };
 
 await build(true);
